fa7d885f5b26f5009a074acfd640f4bf25fb5be4
Miles Florence <milesflo15@gmail.com>
2015-11-19 16:02:57 -0800

    Calculating average time of successful 'get's now works

--- js/app.js
+++ js/app.js
@@ -1,75 +1,84 @@
 //Function runs by itself on page load.
 (function(){
-  $(document).ready(function(){
-
-    imdbConnect();
-    googleBooksConnect();
-    itunesConnect();
-
-  })
+	$(document).ready(function(){
+		imdbConnect();
+		googleBooksConnect();
+		itunesConnect();
+	})
 })();
 
 
 function generateCallback(iteration, apiObj) {
-    return function(){
-        var callbackTime = new Date().getTime();
-        $.ajax(apiObj.url, {success: function(data) {
-            console.log(callbackTime);
-            apiObj.delayTimesCollection.push(new Date().getTime() - callbackTime);
-            apiObj.successfulHits++;
-          }
-          error: function(error) {
-            console.log('failure!');
-            closureMe.failedHits++;
-          } 
-        });
+	return function(){
+		var callbackTime = new Date().getTime();
 
-        //console.log(apiObj); // unchanged
-        //console.log(iteration);
-      }
-    
-}
+		$.ajax(apiObj.url, {
+			success: function(data) {
+				apiObj.delayTimesCollection.push(new Date().getTime() - callbackTime);
+				apiObj.successfulHits++;
+			},
+			error: function(error) {
+				console.log('failure!');
+				apiObj.failedHits++;
+			},
+			done: function() {
+
+			}
+		});
+
+		apiObj.averageDelayTime = calculateAverage(apiObj);
+	};
+};
 
 //IMDB AJAX. Connected to a button.
 function imdbConnect(){
-  $('#imdbButton').on("click", function(){
-    for (var i = 0; i < 100; i++) {
-      setTimeout(generateCallback(i, imdb), 5 * i);
-    };
-  });
+	$('#imdbButton').on("click", function(){
+		for (var i = 0; i < 100; i++) {
+			setTimeout(generateCallback(i, imdb), 5 * i);
+		};
+	});
 };
 
 //Google books AJAX. Connected to a button
 function googleBooksConnect(){
-  $('#googlebooksButton').on("click", function(){
-    for (var i = 0; i < 100; i++) {
-      setTimeout(generateCallback(i, googleBooks), 5 * i);
-    };
-  });
+	$('#googlebooksButton').on("click", function(){
+		for (var i = 0; i < 100; i++) {
+			setTimeout(generateCallback(i, googleBooks), 5 * i);
+		};
+	});
 };
 
 //Itunes AJAX. Connected to button
 function itunesConnect(){
-  $('#itunesButton').on("click", function(){
-    for (var i = 0; i < 100; i++) {
-      setTimeout(generateCallback(i, itunes), 5 * i);
-    };
-  });
+	$('#itunesButton').on("click", function(){
+		for (var i = 0; i < 100; i++) {
+			setTimeout(generateCallback(i, itunes), 5 * i);
+		};
+	});
 };
 
 /*Creator class for new API's. includes name for 
 ease, request type, and url (destination)*/
 var API = function(name, type, url) {
-  this.name = name;
-  this.ajaxStartTime = {};
-  this.dataType = "jsonp";
-  this.type = type;
-  this.url = url;
-  this.successfulHits = 0;
-  this.failedHits = 0;
-  var closureMe = this;
-  this.delayTimesCollection = [];
+	var closureMe = this;
+	this.name = name;
+	this.ajaxStartTime = {};
+	this.dataType = "jsonp";
+	this.type = type;
+	this.url = url;
+	this.successfulHits = 0;
+	this.failedHits = 0;
+	this.delayTimesCollection = [];
 };
+
+var calculateAverage = function(apiObj) {
+			var sum = 0;
+			console.log(apiObj.delayTimesCollection, "THE ARRAY")
+			for (var i = 0; i < apiObj.delayTimesCollection.length; i++) {
+				sum += apiObj.delayTimesCollection[i];
+			};
+			return ((sum /apiObj.delayTimesCollection.length).toFixed(2));
+		};
 
 //Creation of API's using constructor.
 var itunes = new API('iTunes', 'GET', 'https://itunes.apple.com/search?term=beatles');
